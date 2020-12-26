1cdd02dcdc358cd8f4e13def505eecdeeb32dffc
AkisArou <akisarou90@gmail.com>
2020-12-27 01:49:29 +0200

    Now can omit java methods

--- mocks/mockJava.java
+++ mocks/mockJava.java
@@ -1,5 +1,17 @@
-class Animal {
-  public String name;
-  protected int age = 30;
-  private Wife secretWife;
+public class MyBean {
+  private String someProperty;
+  public int someProperty2;
+  protected String someProperty3;
+
+  public String getSomeProperty() {
+    return someProperty;
+  }
+
+  public void setSomeProperty(String someProperty) {
+    this.someProperty = someProperty;
+  }
+
+  public static void someStatic() {
+    return null;
+  }
 }
